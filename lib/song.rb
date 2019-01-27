74cedd08364cc6295d2e91926ea5d134b3ae370b
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-27 19:02:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,89 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    
+   song = self.new
+   song.save
+   song
+  
+  end
+  
+  def self.new_by_name(name)
+    
+   song = self.new
+   song.name = name
+   song.save
+   song
+    
+  end
 
+  def self.create_by_name(name)
+    
+    song = self.new
+    song.name = name
+    song.save
+    song
+    
+  end
+  
+  def self.find_by_name(name)
+    
+    self.all.collect do |song|
+      
+      if song.name == name
+        
+        return song
+        
+      end
+      
+    end
+    
+    return nil
+    
+  end
+  
+  def self.find_or_create_by_name(name)
+    
+    song = find_by_name(name)
+    
+    if song == nil
+
+      
+      song = create_by_name(name)
+    
+    end
+    
+    song
+    
+  end
+  
+  def self.alphabetical
+    
+     self.all.sort_by {|song| song.name}
+   
+  end
+  
+  def self.new_from_filename(file)
+    
+    
+    
+  end
+  
+  def self.create_from_filename(file)
+    
+    
+    
+  end
+  
+  
+  
+  def self.destroy_all
+    
+    self.all.clear
+    
+  end
+  
+  
 end
