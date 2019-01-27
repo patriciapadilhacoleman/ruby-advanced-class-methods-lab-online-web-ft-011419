8e94ae5cc3ba8939768a97e27dddb758ddfa3f6c
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-27 19:12:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -79,7 +79,7 @@ class Song
     array = file.split("-")
     artist = array[0]
     song = create_by_name(array[1].delete('.mp3')
-    song.artist_name = artist
+    @artist_name = artist
   
   end
   
